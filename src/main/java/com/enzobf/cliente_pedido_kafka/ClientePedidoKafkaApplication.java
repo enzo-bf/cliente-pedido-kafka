de970326ec5d2e50cd08349e97ef2da5afe9ed44
enzo-bf <enzobfrancisco08@gmail.com>
2026-09-10 08:09:56 -0300

    chore: initial project setup

---
+++ src/main/java/com/enzobf/cliente_pedido_kafka/ClientePedidoKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.enzobf.cliente_pedido_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientePedidoKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientePedidoKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/enzobf/cliente_pedido_kafka/ClientePedidoKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enzobf.cliente_pedido_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientePedidoKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
