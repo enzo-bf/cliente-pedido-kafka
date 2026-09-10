3b2e1acf3e0c5bf9eaaf2a9d80675fe47738c6d5
enzo-bf <enzobfrancisco08@gmail.com>
2026-09-10 15:06:38 -0300

    feat: add customer retrieval endpoints

--- src/main/java/com/enzobf/cliente_pedido_kafka/controller/ClienteController.java
+++ src/main/java/com/enzobf/cliente_pedido_kafka/controller/ClienteController.java
@@ -1,10 +1,5 @@
 package com.enzobf.cliente_pedido_kafka.controller;
 
-import com.enzobf.cliente_pedido_kafka.dto.request.ClienteRequest;
-import com.enzobf.cliente_pedido_kafka.dto.response.ClienteResponse;
-import com.enzobf.cliente_pedido_kafka.service.ClienteService;
-import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -15,6 +10,13 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.enzobf.cliente_pedido_kafka.dto.request.ClienteRequest;
+import com.enzobf.cliente_pedido_kafka.dto.response.ClienteResponse;
+import com.enzobf.cliente_pedido_kafka.service.ClienteService;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/clientes")
@@ -42,7 +44,7 @@ public class ClienteController {
 
         return ResponseEntity.ok(response);
     }
-
+    
     @GetMapping
     public ResponseEntity<Page<ClienteResponse>> listar(Pageable pageable) {
         Page<ClienteResponse> clientes = clienteService.listar(pageable);
